9748e045e3ebb01862172862f1083877fbe69d36
Stuart Nelson <stuartnelson3@gmail.com>
2013-05-08 21:58:31 -0500

    seems to be working

--- socket_proxy.rb
+++ socket_proxy.rb
@@ -3,11 +3,14 @@ require 'socket'
 class DataDumper
   def initialize name
     @name = name
-    @file = File.open "#{@name << '_' << Time.now.to_s.split[1].gsub(':','_')}.txt", "a"
+    @file = File.open "#{file_name}.txt", "a"
+  end
+
+  def file_name
+    "#{@name << '_' << Time.now.to_s.split[1].gsub(':','_')}"
   end
 
   def log_data data
-    # @file.write "\n#{@name.upcase}:\n"
     @file.write "#{data}\n"
   end
 
@@ -21,35 +24,27 @@ end
 proxy_server = TCPServer.new 8080
 loop do
   Thread.new(proxy_server.accept) do |client|
-    puts 'starting server on port 8080'
+    puts 'connection made'
 
     input_logger = DataDumper.new "input_log"
-    puts 'connecting to endpoint server'
     return_logger = DataDumper.new "return_log"
-    request = ""
     request_logger = DataDumper.new "request_log"
+    request = ""
     while data = client.readline
       puts 'from client:', data
       input_logger.log_data data
       request << data
       if data.strip.empty?
         request_logger.log_data request
-        socket_to_endpoint = TCPSocket.new 'localhost', 4567
+        socket_to_endpoint = TCPSocket.new 'www.google.com', 80
         socket_to_endpoint.print request
         break
       end
-      # received = socket_to_endpoint.read
-      # return_logger.log_data received
-      # socket_to_endpoint.close
     end
 
-    # buff = ""
-    # loop do
-      response = socket_to_endpoint.read #(4048, buff)
-      client.write response #(buff)
-      return_logger.log_data response
-      # break if buff.size < 4048
-    # end
+    response = socket_to_endpoint.readpartial 1024 * 64
+    client.write response
+    return_logger.log_data response
 
     [return_logger, input_logger, request_logger].each(&:end_dump)
     socket_to_endpoint.close
